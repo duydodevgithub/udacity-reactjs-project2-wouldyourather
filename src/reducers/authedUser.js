19c97d1bf520201f5c48fd89f30c34e7ee99f60b
unknown <duydo.dev@gmail.com>
2018-08-29 18:01:25 -0500

    ..

--- src/reducers/authedUser.js
+++ src/reducers/authedUser.js
@@ -3,8 +3,8 @@ import {AUTHENTICATE_USER, SIGN_OUT} from "../actions/types";
 export default function authedUser(state = {}, action) {
     switch (action.type) {
         case AUTHENTICATE_USER:
+            console.log(action.id);
             return action.id
-
         case SIGN_OUT:
             return {}
         default:
